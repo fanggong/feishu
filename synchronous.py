56b798d92a976ee7eb5daa3a9cc8b364ffb0843e
fangyongchao <yongchao.fang@outlook.com>
2024-09-26 14:42:10 +0800

    upadte

--- synchronous.py
+++ synchronous.py
@@ -23,11 +23,11 @@ if __name__ == '__main__':
     public_api = PublicAPI(api_key, secret_key, passphrase, False, '0')
     start_time = int(time.mktime(time.strptime('2024-07-01 00:00:00', '%Y-%m-%d %H:%M:%S'))*1000)
 
-    # for each in ['SPOT', 'SWAP', 'MARGIN']:
-    #     synchronous_instruments(conn, public_api, instType=each)
-    #
-    # synchronous_mark_price(conn=conn, public_api=public_api, inst_type='MARGIN')
+    for each in ['SPOT', 'SWAP', 'MARGIN']:
+        synchronous_instruments(conn, public_api, instType=each)
+
+    synchronous_mark_price(conn=conn, public_api=public_api, inst_type='MARGIN')
     synchronous_bills_history(conn=conn, account_api=account_api, begin=start_time)
-    # synchronous_withdraw_history(conn=conn, funding_api=funding_api)
-    # synchronous_deposit_history(conn=conn, funding_api=funding_api)
+    synchronous_withdraw_history(conn=conn, funding_api=funding_api)
+    synchronous_deposit_history(conn=conn, funding_api=funding_api)
 
