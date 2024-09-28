8a20f1d880dda4cdfc10736727e97393d5619a19
fangyongchao <yongchao.fang@outlook.com>
2024-09-28 18:23:01 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 conda activate feishu
 conda env update --file environment.yml --prune
 cp /root/config.py /srv/feishu/config.py
-pkill -f main.py
+pkill -f "python3 main.py" > /dev/null 2>&1
 nohup python3 main.py > output.log 2>&1 &
