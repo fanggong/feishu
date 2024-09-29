af929fb4878a6539374cb83af791f6bf6fdbd765
fangyongchao <yongchao.fang@outlook.com>
2024-09-29 16:52:50 +0800

    update

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 def event():
     data = request.json
 
-    if data.event['event_key'] == 'crypto_update':
+    if data['event']['event_key'] == 'crypto_update':
         send_text_msg_to_myself(f'[LongQi] [{now()}] 已接收到数据更新请求')
         synchronous_withdraw_history(conn=conn, funding_api=funding_api)
         send_text_msg_to_myself(f'[LongQi] [{now()}] 数据更新成功')
-    elif data.event['event_key'] == 'crypto_report':
+    elif data['event']['event_key'] == 'crypto_report':
         send_text_msg_to_myself(f'[LongQi] [{now()}] 已接收到数据报告请求')
 
     return jsonify({'message': 'Event received'}), 200
