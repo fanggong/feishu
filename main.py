48fdd110ecbbe32ca135053b42ec87bde03ebcc5
fangyongchao <yongchao.fang@outlook.com>
2024-09-29 17:35:33 +0800

    update

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from flask import Flask, request, jsonify
 from utils import *
 from funcs import *
+import asyncio
 
 app = Flask(__name__)
+
+
+async def handle_crypto_update():
+    send_text_msg_to_myself(f'[LongQi] [{now()}] 已接收到数据更新请求')
+    await synchronous_withdraw_history(conn=conn, funding_api=funding_api)
+    send_text_msg_to_myself(f'[LongQi] [{now()}] 数据更新成功')
+
+
+async def handle_crypto_report():
+    send_text_msg_to_myself(f'[LongQi] [{now()}] 已接收到数据报告请求')
 
 
 @app.route('/event', methods=['POST'])
 def event():
     data = request.json
     if data['event']['event_key'] == 'crypto_update':
-        send_text_msg_to_myself(f'[LongQi] [{now()}] 已接收到数据更新请求')
-        synchronous_withdraw_history(conn=conn, funding_api=funding_api)
-        send_text_msg_to_myself(f'[LongQi] [{now()}] 数据更新成功')
+        asyncio.create_task(handle_crypto_update())
     elif data['event']['event_key'] == 'crypto_report':
-        send_text_msg_to_myself(f'[LongQi] [{now()}] 已接收到数据报告请求')
+        asyncio.create_task(handle_crypto_report())
     return jsonify({'message': 'Event received'}), 200
 
 
