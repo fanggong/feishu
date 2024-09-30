b0a5180d40447431f542dadc0ececab97483d627
fangyongchao <yongchao.fang@outlook.com>
2024-09-30 11:11:52 +0800

    update

--- main.py
+++ main.py
@@ -6,15 +6,22 @@ import asyncio
 app = Quart(__name__)
 
 
-async def run_in_back(sync_func, **kwargs):
+async def run_in_back(sync_func):
     loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(None, sync_func, kwargs)
+    return await loop.run_in_executor(None, sync_func)
 
 
 def handle_crypto_update():
-    send_text_msg_to_myself(f'[LongQi] [{now()}] 已接收到数据更新请求')
-    synchronous_withdraw_history(conn=conn, funding_api=funding_api)
-    send_text_msg_to_myself(f'[LongQi] [{now()}] 数据更新成功')
+    send_text_msg_to_myself(f'---------------------Data Update Mission------------------------------')
+    send_text_msg_to_myself(f'| [LongQi] [{now()}] DATA UPDATE MISSION RECEIVED        |')
+    try:
+        for each in ['SPOT', 'SWAP', 'MARGIN']:
+            synchronous_instruments(conn, public_api, instType=each)
+        send_text_msg_to_myself(f'| [LongQi] [{now()}] INSTRUMENTS UPDATE SUCCESS          |')
+    except BaseException as e:
+        send_text_msg_to_myself(f'| [LongQi] [{now()}] INSTRUMENTS UPDATE FAIL             |')
+    send_text_msg_to_myself(f'| [LongQi] [{now()}] DATA UPDATE MISSION COMPLETED       |')
+    send_text_msg_to_myself(f'----------------------------------------------------------------------')
 
 
 def handle_crypto_report():
@@ -22,9 +29,8 @@ def handle_crypto_report():
 
 
 @app.route('/event', methods=['POST'])
-def event():
-    print(request.json)
-    data = request.json
+async def event():
+    data = await request.get_json()
     if data['event']['event_key'] == 'crypto_update':
         asyncio.create_task(run_in_back(handle_crypto_update))
     elif data['event']['event_key'] == 'crypto_report':
