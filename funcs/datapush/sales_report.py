37a1944c3001503d47a325d46d572734c22e32ac
fangyongchao <yongchao.fang@outlook.com>
2024-10-10 16:51:00 +0800

    update

--- funcs/datapush/sales_report.py
+++ funcs/datapush/sales_report.py
@@ -6,10 +6,25 @@ def datapush_sales_report(conn: MysqlEngine, start_date=None, end_date=None, per
     start_date = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d') if not start_date else start_date
     end_date = datetime.now().strftime('%Y-%m-%d') if not end_date else end_date
     period = 1 if not period else period
+
+    values = []
+    graph = {
+        'type': 'bar',
+        'title': {},
+        'data': {
+            'values': values
+        },
+        'xField': 'Date',
+        'yField': 'GMV',
+        'legends': {
+            'visible': True,
+            'orient': 'bottom'
+        }
+    }
     res = {
         'start_date': start_date,
         'end_date': end_date,
         'period': period,
-        'graph': {}
+        'graph': graph
     }
     return res
