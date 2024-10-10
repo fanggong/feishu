bdde2e71d462cf0854579c32d712c68f6275d87c
fangyongchao <yongchao.fang@outlook.com>
2024-10-10 16:41:45 +0800

    update

--- funcs/datapush/sales_report.py
+++ funcs/datapush/sales_report.py
@@ -2,11 +2,14 @@ from database.Mysql import MysqlEngine
 from funcs.utils import *
 
 
-def datapush_sales_report(conn: MysqlEngine):
-    today_m30 = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
-    today = datetime.now().strftime('%Y-%m-%d')
+def datapush_sales_report(conn: MysqlEngine, start_date=None, end_date=None, period=None):
+    start_date = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d') if not start_date else start_date
+    end_date = datetime.now().strftime('%Y-%m-%d') if not end_date else end_date
+    period = 1 if not period else period
     res = {
-        'today_m30': today_m30,
-        'today': today
+        'start_date': start_date,
+        'end_date': end_date,
+        'period': period,
+        'graph': {}
     }
     return res
