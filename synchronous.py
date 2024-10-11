6ea39c5f9c062471b0f8f9e1f04caa257a7bc56c
fangyongchao <yongchao.fang@outlook.com>
2024-10-11 10:17:19 +0800

    update

--- synchronous.py
+++ synchronous.py
@@ -6,5 +6,5 @@ import main
 
 if __name__ == '__main__':
     main.handle_crypto_update()
-    if datetime.datetime.now().minute == 0:
-        main.handle_bar_update()
+    # if datetime.datetime.now().minute == 0:
+    #     main.handle_bar_update()
