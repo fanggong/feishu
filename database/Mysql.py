7b679ff1e17a9ad3acc5cd6d80a8505d90791037
fangyongchao <yongchao.fang@outlook.com>
2024-10-23 15:21:57 +0800

    update

--- database/Mysql.py
+++ database/Mysql.py
@@ -1,13 +1,9 @@
 from sqlalchemy import create_engine, text, MetaData, Table
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects.mysql import insert
 from .utils import *
 
 import pandas as pd
 import numpy as np
-import logging
-import functools
-import time
 
 
 class MysqlEngine:
