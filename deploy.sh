a6c5f296053d98406b93188b14876b5fc22504ec
fangyongchao <yongchao.fang@outlook.com>
2024-11-08 14:08:53 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pkill -f run.py || true
-nohup conda run -n feishu python /srv/feishu/run.py > /srv/feishu/output.log 2>&1 &
+nohup conda run -n feishu python3 /srv/feishu/run.py > /srv/feishu/output.log 2>&1 &
