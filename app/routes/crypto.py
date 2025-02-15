7a0516e6065e00670d49a7019528f139d27dda07
Fang Yongchao <yongchao.fang@outlook.com>
2025-02-15 21:03:38 +0800

    update

--- app/routes/crypto.py
+++ app/routes/crypto.py
@@ -10,19 +10,19 @@ bp = Blueprint('crypto', __name__)
 @bp.route('/event', methods=['POST'])
 async def event():
     data = await request.get_json()
-    # tasks = {
-    #     'crypto_update': handle_crypto_update,
-    #     'crypto_report': handle_crypto_report,
-    #     'risk_report': handle_risk_report
-    # }
-    # if tasks.get(data['event']['event_key']):
-    #     task = asyncio.create_task(run_in_back(tasks.get(data['event']['event_key'])))
-    #     task.add_done_callback(lambda t: t.exception())
-    # return jsonify({'message': 'Event received'}), 200
-    return jsonify({'challenge': data.get('challenge')})
+    tasks = {
+        'crypto_update': handle_crypto_update,
+        'crypto_report': handle_crypto_report,
+        'risk_report': handle_risk_report
+    }
+    if tasks.get(data['event']['event_key']):
+        task = asyncio.create_task(run_in_back(tasks.get(data['event']['event_key'])))
+        task.add_done_callback(lambda t: t.exception())
+    return jsonify({'message': 'Event received'}), 200
+    # return jsonify({'challenge': data.get('challenge')})
 
 
-@bp.route('/webhook', methods=['POSTZ'])
+@bp.route('/webhook', methods=['POST'])
 async def webhook():
     data = await request.get_json()
     return jsonify({'challenge': data.get('challenge')})
